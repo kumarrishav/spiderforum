9ef1bb38ecc84dbccd64eee596502f3b06fef3f1
kumarrishav <reactiverishav007@gmail.com>
2013-10-13 16:14:07 -0700

    anslikecount

---
+++ anslikes.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if(isset($_SESSION['USER']))
+{
+$name=$_SESSION['USER'];//here name instead of id
+$use=$_GET['id'];//ans id
+//echo $use;
+include("connection.php");
+$us=$_GET['te'];
+//echo $us;
+
+$sql1=mysql_query("SELECT alikes FROM anslikes WHERE ansid='$use' AND usessid='$name'");
+$likes=mysql_num_rows($sql1);
+
+if($likes==0)
+{
+$stor="INSERT INTO anslikes (ansid, usessid,alikes) VALUES ('$use','$name','$us')";
+mysql_query($stor);
+}
+else 
+{
+$stor1="UPDATE anslikes SET alikes='$us' WHERE ansid='$use' AND usessid='$name'";
+mysql_query($stor1);
+}
+}
+
+
+
+$count2=mysql_query("SELECT alikes FROM anslikes WHERE alikes='yes' AND ansid='$use'");
+echo $num2=mysql_num_rows($count2);
+?>

25cdd522e6561108b72ad14b23c8d0a6c3a977b3
kumarrishav <reactiverishav007@gmail.com>
2013-10-15 21:25:24 -0700

    every thing is ready+ajax in tags sugggesstion

--- ask.php
+++ ask.php
@@ -10,6 +10,35 @@ left:255;
 }
 
 </style>
+<script>
+var str;
+function showHint(str)
+{
+if (str.length==0)
+  { 
+  document.getElementById("txtHint").innerHTML="";
+  return;
+  }
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+    }
+  }
+xmlhttp.open("GET","gethint.php?q="+str,true);
+xmlhttp.send();
+}
+</script>
+
 </head>
 <body>
 <div class="ques">
@@ -19,7 +48,8 @@ left:255;
 <tr><td>
 <b>Title</b><input type="text" name="title" size="50" placeholder="what's your question?Be specific.."/>
 </br><textarea name="desc" style="width:600px;display:block; float:left;" rows="15"></textarea></br>
-</BR><B>Tags</B></BR><input type="text" name="tags" size="50" placeholder="Minimum one tag & maximun five tags---"/></BR></BR>
+</BR><B>Tags</B></BR><input type="text" name="tags" size="50" onkeyup="showHint(this.value)" placeholder="Minimum one tag & maximun five tags---"/>
+</BR></BR><p>Tags Suggestion:-<span id="txtHint"></span></p>
 <input type="submit" value="Post Your Question">&nbsp;&nbsp;&nbsp;<input type="reset" value="discard">
 </form>
 </div>
---
+++ gethint.php
@@ -0,0 +1,61 @@
+<?php
+// Fill up array with names
+$a[]="Html";
+$a[]="Html5";
+$a[]="Css";
+$a[]="Css3";
+$a[]="JavaScript";
+$a[]="Javs";
+$a[]="Jquery";
+$a[]="Ajax";
+$a[]="Php";
+$a[]="Json";
+$a[]="Sql";
+$a[]="Mysql";
+$a[]="Asp.Net";
+$a[]="Electronics";
+$a[]="Android";
+$a[]="Coding";
+$a[]="Algorithm";
+$a[]="DataStrucure";
+$a[]="FirefoxOs";
+$a[]="Python";
+$a[]="Linux";
+
+//get the q parameter from URL
+$q=$_GET["q"];
+
+//lookup all hints from array if length of q>0
+if (strlen($q) > 0)
+  {
+  $hint="";
+  for($i=0; $i<count($a); $i++)
+    {
+    if (strtolower($q)==strtolower(substr($a[$i],0,strlen($q))))
+      {
+      if ($hint=="")
+        {
+        $hint=$a[$i];
+        }
+      else
+        {
+        $hint=$hint." , ".$a[$i];
+        }
+      }
+    }
+  }
+
+// Set output to "no suggestion" if no hint were found
+// or to the correct values
+if ($hint == "")
+  {
+  $response="no suggestion";
+  }
+else
+  {
+  $response=$hint;
+  }
+
+//output the response
+echo $response;
+?>
